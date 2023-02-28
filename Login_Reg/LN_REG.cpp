b57e37c8462d843fa3119cffda0630d5b5853869
BestChennn <bestchenbestchen@gmail.com>
2023-02-28 21:33:38 +0700

    New

--- Login_Reg/LN_REG.cpp
+++ Login_Reg/LN_REG.cpp
@@ -1,42 +1,14 @@
 #include <iostream>
 #include <string>
 #include <map>
-<<<<<<< HEAD
-#include <fstream>
-#include <vector>
-=======
 #include<fstream>
 #include<vector>
 #include<cstdlib>
 #include<set>
->>>>>>> 6825746765c9565f9714b04cfb59318304ad6f2c
 using namespace std;
 
 // สร้าง map เพื่อเก็บข้อมูลของผู้ใช้งาน
 map<string, string> userDatabase;
-
-<<<<<<< HEAD
-class Database
-{
-public:
-    int num_player;
-    string username, password;
-    char name[100];
-    void registerUser(string, string);
-    void loginUser(string, string);
-    void writeData2_txt(const map<string, string> &, const string &);
-};
-
-// ฟังก์ชั่นเขียนไฟล์ลงไปในไฟล์ DataLN&RG.txt
-void Database::writeData2_txt(const map<string, string> &userDatabase, const string &Datatxt)
-{
-    ofstream dest(Datatxt, std::ios::app);
-
-    for (const auto &[username, password] : userDatabase)
-    {
-        dest << username << " " << password << '\n';
-=======
-
 
 class Database{
     public:
@@ -57,6 +29,7 @@ void Database::Delete_(){
     
     if(file.is_open()){
         while(getline(file,line)){
+
             collet.insert(line);
         }
         file.close();
@@ -64,7 +37,6 @@ void Database::Delete_(){
     
     ofstream newfile("DataLN&RG.txt");
     for(auto it = collet.begin();it != collet.end();++it){
-        
         newfile << *it<<endl;
     }
 newfile.close();
@@ -78,14 +50,11 @@ void Database::writeData2_txt(const map<string,string>&userDatabase, const strin
     
     for(const auto& [username,password]:userDatabase){
         dest<<username << " "<<password<<'\n';
->>>>>>> 6825746765c9565f9714b04cfb59318304ad6f2c
     }
     dest.close();
-}
+    
+    }
 
-<<<<<<< HEAD
-// ฟังก์ชันสำหรับ Login ผู้ใช้งาน //
-=======
 void Database::importDatafromfile(string filename ,vector<string> &un,vector<string> &pw){
 
     ifstream source(filename.c_str());
@@ -112,7 +81,6 @@ string Database::toUpperStr(string x){
 
 
               // ฟังก์ชันสำหรับ Login ผู้ใช้งาน //
->>>>>>> 6825746765c9565f9714b04cfb59318304ad6f2c
 
 //**************************************************************//
 //                                                             //
@@ -120,34 +88,15 @@ string Database::toUpperStr(string x){
 //                                                           //
 //__________________________________________________________//
 
-<<<<<<< HEAD
-void Database::loginUser(string username, string password)
-{
-    // ตรวจสอบว่ามีบัญชีในฐานข้อมูลหรือไม่
-    int num_Userlogin;
-    bool valid = false;
-    // ฟังก์ชั่นเช็คต้องใส่ตัวเลขเท่านั้น
-    while (!valid)
-    {
-        cout << "How UserLogin ? :";
-        cin >> num_Userlogin;
-
-        if (cin.fail())
-        {
-            cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Please enter number !!!\n";
-        }
-        else
-        {
-            valid = true;
-        }
-    }
-=======
 
 void Database::loginUser(string username, string password){   // ตรวจสอบว่ามีบัญชีในฐานข้อมูลหรือไม่
+            
+Database x;
+string filename = "DataLN&RG.txt";
 vector<string> un;
-vector<string> pw;            
+vector<string> pw;
+x.importDatafromfile(filename,un,pw);
+
 int num_Userlogin;
 bool valid = false; 
             while(!valid){                               //ฟังก์ชั่นเช็คต้องใส่ตัวเลขเท่านั้น
@@ -164,46 +113,9 @@ bool valid = false;
                   }
                   
 
->>>>>>> 6825746765c9565f9714b04cfb59318304ad6f2c
 
-    cout << "User Login\n";
+cout<<"User Login\n";
 
-<<<<<<< HEAD
-    for (int i = 1; i <= num_Userlogin; i++)
-    {
-        // ตรวจสอบว่ามีชื่ออยุ่ในฐานข้อมูลไหมน้าาา
-        while (true)
-        {
-            cout << "Username  : ";
-            cin >> username;
-
-            cout << "Couldn't find your username\n";
-            cout << "Please enter your username again.\n";
-        }
-    }
-
-    // ตรวจสอบว่ารหัสผ่านถูกต้องหรือไม่ เช็คจาก map
-    while (true)
-    {
-        cout << "Password : ";
-        cin >> password;
-        if (userDatabase[username] == password)
-        {
-            cout << "User " << username << " has logged in successfully.\n";
-            break;
-        }
-        else
-        {
-            cout << "Incorrect password for user " << username << ".\n";
-        }
-    }
-}
-
-//***************************************************************//
-//                        ระบบ Register 100 %                     //
-//                                                               //
-//***************************************************************//
-=======
 
  for(int i=1;i<=num_Userlogin;i++){   
                                                          //ตรวจสอบว่ามีชื่ออยุ่ในฐานข้อมูลไหมน้าาา
@@ -259,26 +171,15 @@ while(n){
 
 //**************************************************************//
 //                                                             //
-//             >>ระบบRegister   %                              //
-//   ** reg ครั้งต่อมาใส่ชื่อซ้ำได้    **เช็ค ตัวเล็กใหญ่ยังไม่ได้          //
+//             >>ระบบRegister   %                       //
+//                                                           //
 //__________________________________________________________//
->>>>>>> 6825746765c9565f9714b04cfb59318304ad6f2c
+
+
+
 
 // ฟังก์ชันสำหรับ Register ผู้ใช้งาน  // เพิ่มข้อมูลผู้ใช้งานลงในฐานข้อมูล
-<<<<<<< HEAD
-void Database::registerUser(string username, string password)
-{
-
-    string passwordCF;
-    int num_UserRegister;
-    bool valid = false;
-    while (!valid)
-    {
-        cout << "How UserRegister ? : ";
-        cin >> num_UserRegister;
-=======
 void Database::registerUser(string username, string password){
-   
    
     string passwordCF;
     int num_UserRegister;
@@ -286,69 +187,25 @@ void Database::registerUser(string username, string password){
     while(!valid){                                                  //checkต้องnum เท่านั้น
         cout<<"How UserRegister ? : ";
         cin>>num_UserRegister;
->>>>>>> 6825746765c9565f9714b04cfb59318304ad6f2c
 
-        if (cin.fail())
-        {
+        if(cin.fail()){
             cin.clear();
-            cin.ignore(10000, '\n');
-            cout << "Please enter number !!!\n";
-        }
-        else
-        {
+            cin.ignore(10000,'\n');
+            cout<<"Please enter number !!!\n";
+        }else{
             valid = true;
         }
-<<<<<<< HEAD
-    }
-
-    for (int i = 1; i <= num_UserRegister; i++)
-    {
-        while (true)
-        {
-            cout << "User Register\n";
-            cout << "Username : ";
-            cin >> username;
-            if (userDatabase.count(username) > 0)
-            {
-                cout << "That username is taken. Try another";
-            }
-            else
-            {
-                break;
-            }
-        }
-        while (true)
-        {
-            cout << "Password : ";
-            cin >> password;
-            cout << "Confirm Passwowrd : ";
-            cin >> passwordCF;
-            if (password == passwordCF)
-                break;
-            else if (password != passwordCF)
-            {
-                cout << "Passwords didn't match. Try again.\n";
-            }
-        }
-        userDatabase[username] = password;
-        writeData2_txt(userDatabase, "DataLN&RG.txt");
-
-        cout << "User " << username << " has been registered successfully." << endl;
-    }
-=======
 }   
 
 
-cout<<"User Register\n";
+
 
     for(int i=1;i<=num_UserRegister;i++ ){
     bool n = true;
     bool m = true;
-    bool checkUN = false; 
-    bool checkPW = false;
+    cout<<"User Register\n";
     
-    
-     while(true){
+    while(true){
     cout<<"Username : ";
     cin>>username;
      if(userDatabase.count(username)>0 ){
@@ -371,33 +228,23 @@ cout<<"User Register\n";
    else if(password != passwordCF){
     cout<<"Passwords didn't match. Try again.\n";
    }
+
     }
-    userDatabase[username] = password;                                                 //  จัดลำดับให้ตรง user-> pass
-    writeData2_txt(userDatabase,"DataLN&RG.txt");                                      //เขียนinput ลงไปในไฟล์
-    Delete_();                                                                        //ไว้ลบตัวซ้ำ
-    
+    userDatabase[username] = password;
+    writeData2_txt(userDatabase,"DataLN&RG.txt");
+   
     cout << "User " << username << " has been registered successfully." << endl;
     
 }
-    
->>>>>>> 6825746765c9565f9714b04cfb59318304ad6f2c
+    Delete_();
 }
 
-int main()
-{
 
-<<<<<<< HEAD
-    Database x;
-    x.num_player = 4;
-    x.registerUser(x.username, x.password);
-    //   x.loginUser(x.username,x.password);
-=======
 int main() {
     
 Database x;
    
     x.registerUser(x.username, x.password);
    // x.loginUser(x.username,x.password);
->>>>>>> 6825746765c9565f9714b04cfb59318304ad6f2c
     return 0;
 }
