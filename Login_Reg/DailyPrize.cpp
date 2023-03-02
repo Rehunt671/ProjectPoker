aa3bcdb3751034daab787a09cb81c9040d76dba2
640610645 <90751736+640610645@users.noreply.github.com>
2023-03-03 00:30:26 +0700

    dailyPrize V4

--- Login_Reg/DailyPrize.cpp
+++ Login_Reg/DailyPrize.cpp
@@ -1,96 +1,121 @@
 #include <iostream>
+#include <cstring>
 #include <ctime>
 #include <cstdlib>
 #include <fstream>
 using namespace std;
 
-int money = 1000;
+int money = 500; // สมมติให้เงินของใน user ใน database เป็น $500
 
 class check {
-public:
-    bool loggedin;
-    void setBoolLoggedin(bool);
-    void resetBoolLoggedin();
-    void readBoolLoggedin();
-    void writeBoolLoggedin(bool);
+        string filename;
+    public:
+        check();
+        bool loggedin;      // เพิ่มใน Database
+        int resetTime;      // เพิ่มใน Database
+        void setBoolLoggedin(bool);
+        void setResetTime();
+        void getResetTime();
+        void reset();
+        void read();
+        void write(bool,int);
 };
 
-void check::setBoolLoggedin(bool ln) {
+check::check(){
+    filename = "check.txt";        // จำลองไฟล์ DataLN&RG.txt
+}
+
+void check::setBoolLoggedin(bool ln) {    // set ค่า loggedin
     loggedin = ln;
 }
 
-void check::resetBoolLoggedin() {
-    time_t now = time(0);
-    struct tm* t1 = localtime(&now);
-    t1->tm_mday += 1;
-    t1->tm_hour = 0;
-    t1->tm_min = 0;
-    t1->tm_sec = 0;
-    time_t resetTime = mktime(t1); 
-    cout << "current time: " << ctime(&now) << endl;
-    cout << "resetTime: " << ctime(&resetTime) << endl;
-    if (now >= resetTime) {
-        loggedin = false;
+void check::reset() {                     // reset ค่า loggedin ให้เป็น false ทุก ๆ 00.00 น.
+    if (time(0) > resetTime) {
+        setBoolLoggedin(false);
     }
 }
 
-void check::readBoolLoggedin(){
-    ifstream read("check.txt");
-    string boolLN;
-    getline(read,boolLN);
-    if(boolLN == "0") setBoolLoggedin(false);
-    else if(boolLN == "1") setBoolLoggedin(true);
-}
-void check::writeBoolLoggedin(bool ln){
-    ofstream write("check.txt");
-    write << ln;
+void check::write(bool ln,int rst){           // จำลอง writeData2_txt() เขียนค่า loggedin และ resetTime ลงไฟล์
+    ofstream write(filename);
+    write << ln << " " << rst << endl;
+    write.close();
 }
 
-void dailyPrize(check& user) {
-    user.readBoolLoggedin();
-    // user.resetBoolLoggedin(); 
-    if (user.loggedin == false) {
+void check::read(){                      // จำลอง importDatafromfile() อ่านค่า loggedin และ resetTime จากไฟล์
+    ifstream read(filename);
+    string line;
+    char format[] = "%s %s";
+    char boolLoggedin[1];
+    char reset[100];
+    while(getline(read,line)){
+        sscanf(line.c_str(), format, boolLoggedin, reset);
+        if(strcmp(boolLoggedin, "0") == 0) setBoolLoggedin(false);
+        else if(strcmp(boolLoggedin, "1") == 0) setBoolLoggedin(true);
+        resetTime = atoi(reset);
+    }
+    read.close();
+}
+
+void check::setResetTime(){         // set เวลา reset โดยอ้างอิงเวลาของ วันที่ 01/01/23 เวลา 00.00 น.
+    time_t base = 1672506000;       // Thu Jan  1 00:00:00 2023
+    struct tm t1 = *localtime(&base);
+    time_t next = mktime(&t1);
+
+    time_t now = time(0);           // เวลาตอนนี้
+    struct tm t2= *localtime(&now);
+    time_t next2 = mktime(&t2);
+
+    int a = t2.tm_yday - t1.tm_yday;
+
+    time_t rs = base+86400*(a+1);  // เวลา reset
+    resetTime = rs;
+}
+
+void dailyPrize(check& user) {  // fn dailyPrize รับ parameter เป็น class 
+    user.read();    // อ่านค่า loggedin และ resetTime จากไฟล์
+    user.reset();   // เช็คว่าตอนนี้ถึงเวลาที่จะต้อง reset หรือยัง ถ้าถึงเวลาแล้ว loggedin จะถูก reset ให้เป็น false
+    if (user.loggedin == 0 && time(0) > user.resetTime) {      // ถ้า loggedin เป็น false และเวลาตอนนี้เลยเวลา reset มาแล้ว
         cout << "\n------------------------------------------------------------\n";
         cout << "                       DAILY PRIZE\n";
-        cout << "            100 , 200 , 300 , 500 , 750, 1000\n";
+        cout << "            50 , 100, 200 , 300 , 400, 500\n";                   
         cout << "------------------------------------------------------------\n";
         cout << "           Press enter to receive daily prize\n";
         cout << "------------------------------------------------------------";
         cin.get();
         srand(time(0));
-        int i = rand() % 6;
+        int i = rand() % 6;  // prob ของ แต่ละรางวัลเป็น 1/6
         int value;
         switch (i) {
         case 0:
-            value = 100;
+            value = 50;     // +$50
             break;
         case 1:
-            value = 200;
+            value = 100;    // +$100
             break;
         case 2:
-            value = 300;
+            value = 200;    // +$200
             break;
         case 3:
-            value = 500;
+            value = 300;    // +$300
             break;
         case 4:
-            value = 750;
+            value = 400;    // +$400
             break;
         default:
-            value = 1000;
+            value = 500;    // +$500
             break;
         }
-        money += value;
+        money += value;  
         cout << "               You got $" << value << "!\n" << "               Total money: $" << money << endl;
         cout << "------------------------------------------------------------\n";
-        user.setBoolLoggedin(true);
-        user.writeBoolLoggedin(user.loggedin);
+        user.setBoolLoggedin(true);                   // หลังจากบวกเงินของ user แล้ว set ค่า loggedin เป็น true
+        user.setResetTime();                          // set เวลา reset ให้เป็น 00.00 น. ของวันถัดไป
+        user.write(user.loggedin,user.resetTime);     // เขียนค่า loggedin กับ resetTime ลงไฟล์
     }
 }
 
 int main() {
-    check z;
-    z.resetBoolLoggedin();
+    check z;  
     dailyPrize(z);
     return 0;
 }
